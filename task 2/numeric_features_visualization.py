20787d8744be098ad9adb83ce1cdf56fcf639a38
Kanike-tea <kanikekullachanda@gmail.com>
2025-06-25 11:10:02 +0530

    Create histograms and boxplots for numeric features.

---
+++ task 2/numeric_features_visualization.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('/Users/kanike/Documents/ai&ml internship/Elevate-Labs-AI-ML-Internship-Task2/task 2/Titanic-Dataset.csv')
+
+# Histogram
+df.hist(figsize=(10, 8), bins=20)
+plt.suptitle("Histograms of Numerical Features", fontsize=16)
+plt.tight_layout()
+plt.show()
+
+# Boxplot for each numerical column
+numeric_cols = df.select_dtypes(include='number').columns
+for col in numeric_cols:
+    plt.figure(figsize=(6, 2.5))
+    sns.boxplot(x=df[col])
+    plt.title(f"Boxplot of {col}")
+    plt.show()
