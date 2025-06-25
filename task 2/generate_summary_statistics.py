c4d575c47b08c63b903a7e0ac495c7e1c646558c
Kanike-tea <kanikekullachanda@gmail.com>
2025-06-25 11:06:44 +0530

    Generate summary statistics (mean, median, mode and standard deviation)

---
+++ task 2/generate_summary_statistics.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df = pd.read_csv('/Users/kanike/Documents/ai&ml internship/Elevate-Labs-AI-ML-Internship-Task2/task 2/Titanic-Dataset.csv')
+
+# Summary statistics for numerical columns
+print(df.describe())
+
+# Select only numeric columns
+numeric_df = df.select_dtypes(include='number')
+
+# Mean
+print("\nMean:\n", numeric_df.mean(numeric_only=True), "\n")
+
+# Median
+print("Median:\n", numeric_df.median(numeric_only=True), "\n")
+
+# Mode (may have multiple values per column)
+print("Mode:\n", numeric_df.mode(numeric_only=True).iloc[0], "\n")
+
+# Standard Deviation
+print("Standard Deviation:\n", numeric_df.std(numeric_only=True), "\n")
