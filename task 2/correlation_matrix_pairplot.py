009cd750f8360026b094c1464d0f6c5f1f51d827
Kanike-tea <kanikekullachanda@gmail.com>
2025-06-25 17:51:02 +0530

    Use pairplot/correlation matrix for feature relationships.

---
+++ task 2/correlation_matrix_pairplot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('/Users/kanike/Documents/ai&ml internship/Elevate-Labs-AI-ML-Internship-Task2/task 2/Titanic-Dataset.csv')
+
+#correlation matrix
+plt.figure(figsize=(10, 8))
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title("Correlation Matrix")
+plt.show()
+
+# Pairplot for a smaller set of numerical features
+selected_cols = ['Age', 'Fare', 'Pclass', 'Survived']
+sns.pairplot(df[selected_cols], hue='Survived')
+plt.suptitle("Pairplot of Selected Features", y=1.02)
+plt.show()
